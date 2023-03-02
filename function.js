ca344fe192d93f75df7e3915c44c4d72a52fc153
Francary <francarymolinapaez@gmail.com>
2023-03-02 10:49:33 -0300

    Entrega CV

---
+++ function.js
@@ -0,0 +1,17 @@
+document.getElementById('modo-oscuro').addEventListener('click', function()
+{
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+    //document.getElementById('container').style.backgroundColor = '#666666' Me falto implentar que se cambien los colores de los container
+    
+});
+
+document.getElementById('modo-claro').addEventListener('click', function()
+{
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+    //document.getElementById('container').style.backgroundColor = '#666666' Me falto implentar que se cambien los colores de los container
+    
+});
+
+
